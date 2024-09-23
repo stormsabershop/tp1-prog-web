62f5fdd6fe059250cda6d9903957214b755920ba
Jerome Ouellet1 <2346784@cegeplimoilou.ca>
2024-09-23 15:12:41 -0400

    premier commit par jerome

---
+++ Semaine 5 - Tp 1 - JS - Questionnaire/Noms des équipiers – Tp 1-JeuQuestionnaire – A24/data/questionsJSON.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//IMPORTANT : Pour la syntaxe JSON voir ce lien : https://www.json.org/json-fr.html
+
+//Mettre vos questions au format JSON ici. Un seul objet principal pour toutes les questions
+
+//Faites quelque chose de simple comme on a vu ensemble.
+
+let tabAssQuestions = { };
---
+++ Semaine 5 - Tp 1 - JS - Questionnaire/Noms des équipiers – Tp 1-JeuQuestionnaire – A24/js/tp1-JeuQuestionnaire.js
@@ -0,0 +1,3 @@
+"use strict"
+
+//Mettre votre code JS ici
